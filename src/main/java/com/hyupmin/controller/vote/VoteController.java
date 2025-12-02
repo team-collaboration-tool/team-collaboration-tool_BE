dfb171b22a18a0df26d92b5b3f60b9348c4132ab
JunHwan Jeon <hwan6274@naver.com>
2025-12-02 15:17:33 +0900

    feat: VoteController에 투표하기 기능 생성

--- src/main/java/com/hyupmin/controller/vote/VoteController.java
+++ src/main/java/com/hyupmin/controller/vote/VoteController.java
@@ -33,6 +33,20 @@ public class VoteController {
     }
 
     /**
+    * 투표하기 (생성 후 첫 투표)
+     * [Post] /api/votes/options/{optionId}/cast
+     * 설명: 투표 생성 후 처음 투표하기 기능을 사용했을 때 작동합니다.
+     * */
+    @PostMapping("/options/{optionId}/cast")
+    public ResponseEntity<String> castVote(
+            @AuthenticationPrincipal String userEmail,
+            @PathVariable Long optionId
+    ) {
+        voteService.castVote(optionId, userEmail);
+        return ResponseEntity.ok("투표가 완료되었습니다.");
+    }
+
+    /**
      * 재투표하기 (기존 투표 취소 후 다시 투표)
      * [PUT] /api/votes/options/{optionId}/cast
      * 설명: 기존에 투표한 내역이 있다면 삭제하고, 새로운 optionId로 다시 투표합니다.
